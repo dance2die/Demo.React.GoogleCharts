811e204d2afdac2dc380e397af2eecc18a73c8fb
dance2die <dontbothermewithspam@gmail.com>
2017-07-02 01:26:53 -0400

    added axios & proxify-url npm packages. And also implemented calling GoodReads API for getting usershelf data

--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,42 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Chart } from 'react-google-charts';
 import TimeLineChart from './timeline_chart';
+import ShowGoodReads from './show_goodreads';
+import axios from 'axios';
+import defiant from 'defiant';
+import apiConfig from '../../apikeys';
+import proxify from 'proxify-url';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.goodReadsURL = `https://www.goodreads.com/shelf/list.xml`;
+    this.q = `key=${apiConfig.goodreadsKey}&user_id=${apiConfig.goodreadsUserID}&page=1`;
+    this.url = proxify(`${this.goodReadsURL}?${this.q}`, { inputFormat: 'xml' });
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // Get GoodReads data via Web API.
+
+
+    axios.get(this.url, { params: { format: "json" } })
+      .then((yqlResponse) => {
+        let data = yqlResponse.data;
+
+        // DefiantJS XPath query for user shelf for "read" section.
+        let search = JSON.search(data, "//*/user_shelf[name='read']");
+
+        this.setState({ userShelf: search });
+
+        // console.log(search);
+      }).catch((error) => {
+        alert(`error: ${error}`);
+      });
+  }
+
   render() {
     return (
       <div className={'my-pretty-chart-container'}>
@@ -17,6 +51,7 @@ export default class App extends Component {
           legend_toggle
         />
         <TimeLineChart />
+        <ShowGoodReads userShelf={this.state.userShelf} />
       </div>
     );
   }
---
+++ src/components/show_goodreads.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class ShowGoodReads extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        console.log(this.props.userShelf);
+        
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
