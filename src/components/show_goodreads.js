d4a90ec3478110946b813e65aaa6306c3145d82e
dance2die <dontbothermewithspam@gmail.com>
2017-07-02 09:03:14 -0400

    finally Google Charts Timeline shows books!!!

--- src/components/show_goodreads.js
+++ src/components/show_goodreads.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Chart } from 'react-google-charts';
 
 export default class ShowGoodReads extends Component {
     constructor(props) {
@@ -24,28 +25,87 @@ class ShelfBooks extends Component {
         this.state = {};
     }
 
+    componentWillReceiveProps(nextProps) {
+        // console.log('nextProps', nextProps);
+        // if (!this.props.shelfBooks) {
+        //     console.log("componentWillReceiveProps: there is no shelfBooks data, yet");
+        //     return;
+        // }
+
+        let books = nextProps.shelfBooks
+            .filter(shelfBook => {
+                return Date.parse(shelfBook.started_at) < Date.parse(shelfBook.read_at);
+            })
+            .map(shelfBook => {
+                console.log('shelfBook:', shelfBook);
+                let result = [
+                    shelfBook.book.title,
+                    new Date(Date.parse(shelfBook.started_at)),
+                    new Date(Date.parse(shelfBook.read_at))
+                ];
+
+                console.group();
+                console.log('result:', result);
+                console.groupEnd();
+
+
+                return result;
+            });
+
+        console.log('componentWillReceiveProps: books', books);
+
+        this.setState({
+            "chartType": "Timeline",
+            "columns": [
+                { "id": "BookTitle", "type": "string" },
+                { "id": "StartAt", "type": "date" },
+                { "id": "EndAt", "type": "date" },
+            ],
+            "rows": books,
+            "options": { "width": "600px" },
+            "width": "100%",
+            "height": "800px",
+            "graph_id": "bookShelfTimeLine",
+            "chartPackage": "timeline"
+        });
+    }
+
     render() {
-        if (!this.props.shelfBooks) {
-            console.log("there is no shelfBooks data, yet");
+        // if (!this.props.shelfBooks) {
+        if (!this.state.chartType) {
+            console.log("there is no state, yet....");
             return <div>there is no shelfBooks data</div>;
         }
 
-        console.log('this.props.shelfBooks', this.props.shelfBooks);
+        console.log('render: this.state', this.state);
 
-        let books = this.props.shelfBooks.map(shelfBook => {
-            return (
-                <div key={shelfBook.id}>
-                    <div>Title:</div><div>{shelfBook.book.title}</div>
-                    <div>Started At:</div><div>{shelfBook.started_at}</div>
-                    <div>Read At:</div><div>{shelfBook.read_at}</div>
-                    <br />
-                    <br />
-                </div >
-            );
-        });
+        // let books = this.props.shelfBooks.map(shelfBook => {
+        //     return (
+        //         <div key={shelfBook.id}>
+        //             <div>Title:</div><div>{shelfBook.book.title}</div>
+        //             <div>Started At:</div><div>{shelfBook.started_at}</div>
+        //             <div>Read At:</div><div>{shelfBook.read_at}</div>
+        //             <br />
+        //             <br />
+        //         </div >
+        //     );
+        // });
+
+
+        let chart = <Chart
+            chartType={this.state.chartType}
+            rows={this.state.rows}
+            columns={this.state.columns}
+            options={this.state.options}
+            width={this.state.width}
+            chartPackage={this.state.chartPackage}
+            graph_id={this.state.graph_id}
+            height={this.state.height}
+            legend_toggle
+        />;
 
         return (
-            <div>{books}</div>
+            <div>{chart}</div>
         );
     }
 }
