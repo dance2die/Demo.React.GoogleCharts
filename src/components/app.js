f632711f78475c92e26819fc317893577f473808
dance2die <dontbothermewithspam@gmail.com>
2017-07-01 22:22:52 -0400

    trying a scatter chart

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Chart } from 'react-google-charts';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div className={'my-pretty-chart-container'}>
+        <Chart
+          chartType="ScatterChart"
+          data={[['Age', 'Weight'], [8, 12], [4, 5.5]]}
+          options={{}}
+          graph_id="ScatterChart"
+          width="100%"
+          height="400px"
+          legend_toggle
+        />
+      </div>
     );
   }
 }
